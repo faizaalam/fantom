580605fa4fbf5f94190cc13879a7ee3f5ac6b845
faiza <faiza@mislbd.com>
2022-02-20 10:45:28 +0600

    Initial commit

---
+++ fantom/src/main/java/com/blockchain/fantom/FantomApplication.java
@@ -0,0 +1,13 @@
+package com.blockchain.fantom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantomApplication.class, args);
+	}
+
+}
---
+++ fantom/src/test/java/com/blockchain/fantom/FantomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockchain.fantom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
